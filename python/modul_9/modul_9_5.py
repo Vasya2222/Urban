091acfa85f3c14ac5fcd24da04ae9fc9e9e45818
vasya2222 <vasyaryzhov2906@gmail.com>
2024-11-15 13:04:05 +0300

    add homework 9 for 5 lesson

---
+++ python/modul_9/modul_9_5.py
@@ -0,0 +1,40 @@
+class StepValueError(ValueError):
+    pass
+
+
+class Iterator:
+    def __init__(self, start, stop, step=1):
+        if step == 0:
+            raise StepValueError('шаг не может быть равен 0')
+        self.start = start
+        self.stop = stop
+        self.step = step
+        self.pointer = start
+
+    def __iter__(self):
+        self.pointer = self.start
+        return self
+
+    def __next__(self):
+        if self.step > 0 and self.pointer > self.stop:
+            raise StopIteration()
+        if self.step < 0 and self.pointer < self.stop:
+            raise StopIteration()
+        current_value = self.pointer
+        self.pointer += self.step
+        return current_value
+
+
+iter2 = Iterator(-5, 1)
+iter3 = Iterator(6, 15, 2)
+iter4 = Iterator(5, 1, -1)
+iter5 = Iterator(10, 1)
+for i in iter2:
+    print(i, end=' ')
+print()
+for i in iter3:
+    print(i, end=' ')
+print()
+for i in iter4:
+    print(i, end=' ')
+print()
